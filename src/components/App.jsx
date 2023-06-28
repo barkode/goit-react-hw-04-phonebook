277c6263a533c2a91f8142540a136cb8c65aafeb
barkode <konstantinks@gmail.com>
2023-06-28 01:46:46 +0100

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,40 +7,25 @@ import css from './App.module.css';
 
 const LS_KEY = 'contacts';
 
-const contactList = [
-  { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-  { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-  { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-  { id: 'id-4', name: 'Jack Jones', number: '975-24-89' },
-  { id: 'id-5', name: 'Captain Morgan', number: '227-90-26' },
-  { id: 'id-6', name: 'Bilie Joe', number: '217-95-26' },
-  { id: 'id-7', name: 'Joe Foster', number: '297-99-26' },
-  { id: 'id-8', name: 'Samuel El Jackson', number: '287-04-26' },
-  { id: 'id-9', name: 'Annie Milligan', number: '154-44-28' },
-]
-
-
-export function App () {
+export function App() {
   const [contacts, setContacts] = useState([]);
   const [filter, setFilter] = useState('');
 
   useEffect(() => {
-    const contactKey = localStorage.getItem(LS_KEY);
-    const parseContacts = JSON.parse(contactKey);
-    if (parseContacts && parseContacts.length !== 0) {
+    const parseContacts = JSON.parse(localStorage.getItem(LS_KEY));
+    if (parseContacts) {
       setContacts(parseContacts);
-      return;}
-    localStorage.setItem(LS_KEY, JSON.stringify(contactList))
-    }, [])
+      return;
+    }
+  }, []);
 
+  useEffect(() => {
+    // contacts.length !== 0 &&
+    localStorage.setItem(LS_KEY, JSON.stringify(contacts));
+  }, [contacts]);
 
-    useEffect(() => {
-      (contacts.length !== 0) && localStorage.setItem(LS_KEY, JSON.stringify(contacts));
-  }, [contacts])
-
-
-    const handleFilterChange = e => {
-    setFilter(e.target.value );
+  const handleFilterChange = e => {
+    setFilter(e.target.value);
   };
 
   const handleAddContact = newContact => {
@@ -53,11 +38,13 @@ export function App () {
       return;
     }
     setContacts([...contacts, newContact]);
-    setFilter('')
+    setFilter('');
   };
 
   const handleDeleteContact = id => {
-    setContacts(() => (contacts.filter(contact => contact.id !== id)))
+    setContacts(prev => {
+      return prev.filter(contact => contact.id !== id);
+    });
   };
 
   const handleFilterAbonent = () => {
@@ -68,19 +55,19 @@ export function App () {
     });
   };
 
-    return (
-      <div className={css.container}>
-        <h1>Phone book</h1>
-        <ContactForm onAddContact={handleAddContact} />
-        <h2>Contacts</h2>
-        <Filter filter={filter} onFilterChange={handleFilterChange} />
-        <ContactList
-          contacts={filter ? handleFilterAbonent() : contacts}
-          handleDeleteContact={handleDeleteContact}
-        />
-      </div>
-    );
-  }
+  return (
+    <div className={css.container}>
+      <h1>Phone book</h1>
+      <ContactForm onAddContact={handleAddContact} />
+      <h2>Contacts</h2>
+      <Filter filter={filter} onFilterChange={handleFilterChange} />
+      <ContactList
+        contacts={filter ? handleFilterAbonent() : contacts}
+        handleDeleteContact={handleDeleteContact}
+      />
+    </div>
+  );
+}
 
 App.propTypes = {
   contacts: PropType.arrayOf(
